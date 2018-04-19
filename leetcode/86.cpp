2cb8526f29571309039bcc3538a1345cbb59b8c3
ylink <38040668+ylink-lfs@users.noreply.github.com>
2018-04-19 16:14:27 +0800

    Create 86.cpp

---
+++ leetcode/86.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) 
+    {
+        if(!head)
+            return nullptr;
+        ListNode* dummynode = new ListNode(INT_MIN);
+        dummynode->next = head;
+        ListNode* pos = head;
+        while(pos && pos->val < x)
+            pos = pos->next;
+        if(!pos)
+            return head;
+        ListNode* current = pos;
+        ListNode* temp = nullptr;
+        ListNode* pnew = nullptr;
+        do
+        {
+            while(current && current->val >= x)
+                current = current->next;
+            if(!current)
+                break;
+            pnew = new ListNode(current->val);
+            temp = findPrevious(dummynode, pos);
+            pnew->next = pos;
+            temp->next = pnew;
+            temp = findPrevious(dummynode, current);
+            temp->next = current->next;
+            current = pos;
+        }while(current);
+        return dummynode->next;
+    }
+    ListNode* findPrevious(ListNode* head, ListNode* node)
+    {
+        ListNode* current = head;
+        while(current->next != node)
+        {
+            current = current->next;
+        }
+        return current;
+    }
+};
