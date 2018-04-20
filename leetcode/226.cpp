6da296cae3f9cd3fa4039d1174478af8b0d6b587
ylink <38040668+ylink-lfs@users.noreply.github.com>
2018-04-20 09:21:46 +0800

    Create 226.cpp

---
+++ leetcode/226.cpp
@@ -0,0 +1,19 @@
+//lol
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        if(root == nullptr)
+            return root;
+        else       
+        {
+            TreeNode* temp;
+            temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+        }
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+        return root;
+    }
+};
