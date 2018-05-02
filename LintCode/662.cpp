3877328e2c9f22e9b542fbea4b1d9708f0e7eb75
ylink <38040668+ylink-lfs@users.noreply.github.com>
2018-05-02 15:28:38 +0800

    Create 662.cpp

---
+++ LintCode/662.cpp
@@ -0,0 +1,22 @@
+int guessNumber(int n) {
+        int mid;
+        int lower = 1;
+        int upper = n;
+        while(lower <= upper)
+        {
+            mid = lower + (upper - lower) / 2;
+            if(guess(mid) == 0)
+            {
+                return mid;
+            }
+            else if (guess(mid) == -1)
+            {
+                upper = mid - 1;
+            }
+            else
+            {
+                lower = mid + 1;
+            }
+        }
+        return -1;
+    }
