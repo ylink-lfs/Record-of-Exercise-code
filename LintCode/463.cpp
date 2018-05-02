2fa58770a1d26bb54e2883a59f9f2a52a3afe2d0
ylink <38040668+ylink-lfs@users.noreply.github.com>
2018-05-02 15:57:20 +0800

    Create 463.cpp

---
+++ LintCode/463.cpp
@@ -0,0 +1,11 @@
+//Just for fun :-)
+class Solution {
+public:
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    void sortIntegers(vector<int> &A) {
+        std::sort(A.begin(), A.end());
+    }
+};
