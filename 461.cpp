0a2ec8da22e5c95e818ccac74f4c53b0dc6f7950
ylink <38040668+ylink-lfs@users.noreply.github.com>
2018-05-06 09:43:35 +0800

    Create 461.cpp

---
+++ 461.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int board = x ^ y;
+        int counter = 0;
+        while(board != 0)
+        {
+            board &= (board - 1);
+            counter++;
+        }
+        return counter;
+    }
+};
