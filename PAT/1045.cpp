d415c81bca6e7ae2924018c1102753c5f667a4a1
ylink <lfs@live.com>
2019-01-28 19:58:45 +0800

    Update 1045.cpp

--- PAT/1045.cpp
+++ PAT/1045.cpp
@@ -64,3 +64,50 @@ int main(void)
 	}
 	return 0;
 }
+
+//LCS Solution
+//#include <iostream>
+//#include <algorithm>
+//#include <cstdlib>
+//
+//int dp[210][10010];
+//int s1[210];
+//int s2[10010];
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	int tot_colors;
+//	cin >> tot_colors;
+//	int color_count;
+//	cin >> color_count;
+//	for (int i = 0; i < color_count; i++)
+//	{
+//		cin >> s1[i];
+//	}
+//	int stripe_len;
+//	cin >> stripe_len;
+//	for (int i = 0; i < stripe_len; i++)
+//	{
+//		cin >> s2[i];
+//	}
+//	for (int i = 0; i < color_count; i++)
+//	{
+//		for (int j = 0; j < stripe_len; j++)
+//		{
+//			if (s1[i] == s2[j])
+//			{
+//				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]) + 1;
+//			}
+//			else
+//			{
+//				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+//			}
+//		}
+//	}
+//	cout << dp[color_count][stripe_len] << '\n';
+//	return 0;
+//}
