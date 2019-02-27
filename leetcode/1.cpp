5725b36e1ba7a2cbc57d5d9e089bbcc5265a1f1c
ylink <lfs@live.com>
2019-02-27 12:53:19 +0800

    Create 1.cpp

---
+++ leetcode/1.cpp
@@ -0,0 +1,1 @@
+class Solution{public:vector<int> twoSum(vector<int>& nums, int target){int n=nums.size(),l=0,r=n-1;unordered_map<int, int> mp;for(int i=0;i<n;i++){if (mp.find(target-nums[i])!=mp.end()){l=mp[target-nums[i]];r=i;break;}mp[nums[i]]=i;}return{l,r};}};
