58850e7aa2108aa6c90e7b1dda38dac36b23cf3a
ylink <lfs@live.com>
2020-03-27 08:24:15 +0800

    Update 1.cpp

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,1 +1,23 @@
-class Solution{public:vector<int> twoSum(vector<int>& nums, int target){int n=nums.size(),l=0,r=n-1;unordered_map<int, int> mp;for(int i=0;i<n;i++){if (mp.find(target-nums[i])!=mp.end()){l=mp[target-nums[i]];r=i;break;}mp[nums[i]]=i;}return{l,r};}};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        mp.insert(make_pair(nums[0], 0));
+        vector<int> result;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            int component_num = target - nums[i];
+            if(mp.find(component_num) != mp.end())
+            {
+                result.push_back(mp.at(component_num));
+                result.push_back(i);
+                break;
+            }
+            else
+            {
+                mp.insert(make_pair(nums[i], i));
+            }
+        }
+        return result;
+    }
+};
